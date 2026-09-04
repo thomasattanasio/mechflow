7f0a7d3f477c6b61215cc77a4378278f03b60b33
thomasattanasio <tattanasio.dev@gmail.com>
2026-09-04 23:31:57 +0200

    Setup virtual environment and install core dependencies

---
+++ app/database/database.py
@@ -0,0 +1,32 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+# Database URL configuration
+SQLALCHEMY_DATABASE_URL = 'sqllite:///./mechfflow.db'
+
+
+# Create the SQLAlchemy engine
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}
+)
+
+
+# Create a session local class
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+# Create a base class for models
+Base = declarative_base()
+
+
+# Dependency to get the database session in routers
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+
+    finally:
+        db.close()
